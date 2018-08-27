3532129081e323676193ce997f27222b61b38fec
Emily Horsman <me@emilyhorsman.com>
2018-08-27 09:03:22 -0700

    Start new Xcode Cocoa project for a status bar app

---
+++ TimerSwift/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TimerSwift
+//
+//  Created by Emily Horsman on 8/27/18.
+//  Copyright © 2018 Emily Horsman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
