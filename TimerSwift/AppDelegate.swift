33954ecdb90446cd7dfd79794f5d703a05c36298
Emily Horsman <me@emilyhorsman.com>
2018-08-27 10:17:22 -0700

    Move AppDelegate logic into StatusMenuController

--- TimerSwift/AppDelegate.swift
+++ TimerSwift/AppDelegate.swift
@@ -8,57 +8,11 @@
 
 import Cocoa
 
-enum TimerState {
-    case Stopped, Running
-}
-
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var statusMenu: NSMenu!
-    @IBOutlet weak var timerMenuItem: NSMenuItem!
-
-    var timerState: TimerState = .Stopped
-
-    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-    let appTimer = AppTimer()
-
-    @IBAction func quitClicked(_ sender: NSMenuItem) {
-        NSApp.terminate(self)
-    }
-
-    @IBAction func timerClicked(_ sender: NSMenuItem) {
-        switch timerState {
-        case .Running:
-            stop()
-        case .Stopped:
-            start()
-        }
-    }
-
-    func start() {
-        timerState = .Running
-        timerMenuItem.title = "Stop"
-        appTimer.reset()
-    }
-
-    func stop() {
-        timerState = .Stopped
-        timerMenuItem.title = "Start"
-        statusItem.title = "Timer"
-    }
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         NSApp.setActivationPolicy(.prohibited)
-
-        statusItem.title = "Timer"
-        statusItem.menu = statusMenu
-
-        appTimer.addTickHandler { (interval: TimeInterval) -> Void in
-            if self.timerState == .Running, let label = StatusBarDateComponentsFromatter.string(from: interval) {
-                self.statusItem.title = label
-            }
-        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
---
+++ TimerSwift/StatusMenuController.swift
@@ -0,0 +1,59 @@
+//
+//  StatusMenuController.swift
+//  TimerSwift
+//
+//  Created by Emily Horsman on 8/27/18.
+//  Copyright © 2018 Emily Horsman. All rights reserved.
+//
+
+import Cocoa
+
+enum TimerState {
+    case Stopped, Running
+}
+
+class StatusMenuController: NSObject {
+    @IBOutlet weak var statusMenu: NSMenu!
+    @IBOutlet weak var timerMenuItem: NSMenuItem!
+
+    var timerState: TimerState = .Stopped
+
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    let appTimer = AppTimer()
+
+    @IBAction func quitClicked(_ sender: NSMenuItem) {
+        NSApp.terminate(self)
+    }
+
+    @IBAction func timerClicked(_ sender: NSMenuItem) {
+        switch timerState {
+        case .Running:
+            stop()
+        case .Stopped:
+            start()
+        }
+    }
+
+    func start() {
+        timerState = .Running
+        timerMenuItem.title = "Stop"
+        appTimer.reset()
+    }
+
+    func stop() {
+        timerState = .Stopped
+        timerMenuItem.title = "Start"
+        statusItem.title = "Timer"
+    }
+
+    override func awakeFromNib() {
+        statusItem.title = "Timer"
+        statusItem.menu = statusMenu
+
+        appTimer.addTickHandler { (interval: TimeInterval) -> Void in
+            if self.timerState == .Running, let label = StatusBarDateComponentsFromatter.string(from: interval) {
+                self.statusItem.title = label
+            }
+        }
+    }
+}
