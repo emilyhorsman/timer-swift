efb9e6796f9d7e0cc76a004b1f63fbfc3b8de6da
Emily Horsman <me@emilyhorsman.com>
2018-08-28 13:59:07 -0700

    Start from scratch with a PreferencesWindowController

---
+++ TimerSwift/PreferencesWindowController.swift
@@ -0,0 +1,18 @@
+//
+//  PreferencesWindowController.swift
+//  TimerSwift
+//
+//  Created by Emily Horsman on 8/28/18.
+//  Copyright © 2018 Emily Horsman. All rights reserved.
+//
+
+import Cocoa
+
+class PreferencesWindowController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("View Did Load!")
+    }
+    
+}
