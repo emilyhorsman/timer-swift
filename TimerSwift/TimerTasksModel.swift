36d938b5fada7a913919e79bad9447c9dca8781d
Emily Horsman <me@emilyhorsman.com>
2018-09-02 19:20:19 -0400

    Prepend instead of append log
    This makes it kind of easiser to read…even though it's not necessary

--- TimerSwift/TimerTasksModel.swift
+++ TimerSwift/TimerTasksModel.swift
@@ -51,7 +51,6 @@ class TimerTasksModel: NSObject {
             defer {
                 handle.closeFile()
             }
-            handle.seekToEndOfFile()
             handle.write(rowData)
         } catch {
             print(error)
