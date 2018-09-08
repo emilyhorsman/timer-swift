14163601e882f2abb2e0d5f94a7dd07e081892fd
Emily Horsman <me@emilyhorsman.com>
2018-09-08 18:18:48 -0400

    Show time I reached 4 hours in sum.py

--- sum.py
+++ sum.py
@@ -7,6 +7,7 @@ with open(sys.argv[1], 'r') as f:
     lines = contents.split('\n')
 
 sum = dict()
+reached = dict()
 for line in lines:
     if line.count(',') != 2:
         continue
@@ -18,6 +19,11 @@ for line in lines:
     if key not in sum:
         sum[key] = datetime.timedelta()
     sum[key] += interval
+    if sum[key] >= datetime.timedelta(hours=4) and key not in reached:
+        reached[key] = end - (sum[key] - datetime.timedelta(hours=4))
 
 for key, value in sum.items():
-    print(key, value)
+    if key in reached:
+        print(key, value, '4h by', reached[key].strftime('%-I:%M%p'))
+    else:
+        print(key, value)
