eb1f82dc2d19a3c3abe4b55015fa903314df76fa
Emily Horsman <me@emilyhorsman.com>
2019-01-06 10:57:35 -0800

    Display totals in sum.py table output

--- sum.py
+++ sum.py
@@ -11,6 +11,7 @@ with open(sys.argv[1], 'r') as f:
 codes = set()
 sum = dict()
 reached = dict()
+per_course = dict()
 total = 0
 for line in lines:
     if line.count(',') != 2:
@@ -24,11 +25,14 @@ for line in lines:
         sum[key] = dict(total=datetime.timedelta())
     if code not in sum[key]:
         sum[key][code] = datetime.timedelta()
+    if code not in per_course:
+        per_course[code] = 0
     sum[key][code] += interval
     sum[key]['total'] += interval
     if sum[key]['total'] >= datetime.timedelta(hours=4) and key not in reached:
         reached[key] = end - (sum[key]['total'] - datetime.timedelta(hours=4))
     total += interval.seconds
+    per_course[code] += interval.seconds
     codes.add(code)
 
 table = OrderedDict([('Date', []), ('Total', []), ('4h By', [])])
@@ -46,10 +50,23 @@ for key, value in sum.items():
     else:
         table['4h By'].append('-')
 
+table['Date'].append('')
+table['Total'].append('')
+table['4h By'].append('')
+for code in sorted(codes):
+    table[code].append(code)
+
 table['Date'].append('Total')
 table['Total'].append(total // 60 // 60)
 table['4h By'].append('')
 for code in sorted(codes):
-    table[code].append('')
+    table[code].append(per_course[code] // 60 // 60)
+
+table['Date'].append('')
+table['Total'].append('')
+table['4h By'].append('')
+for code in sorted(codes):
+    table[code].append("{:.1f}%".format(per_course[code] / total * 100))
+
 
 print(tabulate(table, headers="keys", stralign="right"))
